154b19675667d3ad9499c6ae7cbe89881eb4b521
kretgit <kret86@yandex.ru>
2021-07-08 00:52:41 +0300

    items files ready...

--- src/components/pages/home-page.js
+++ src/components/pages/home-page.js
@@ -1,7 +1,15 @@
 import React from "react";
+import BookList from "../book-list";
 
 const HomePage = () => {
-    return <div>Home Page</div>
+    const books = [
+        {id: 1, title: 'Dracula', author: 'Bram Stoker'},
+        {id: 2, title: 'React Redux TypeScript', author: 'o Relly'}
+    ];
+
+    return (
+        <BookList books={books}/>
+    )
 };
 
 export default HomePage;
