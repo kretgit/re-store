3cd901baa8ba43a14379155c7e1c1036abbd0a39
kretgit <kret86@yandex.ru>
2021-07-08 00:32:13 +0300

    redux files ready...

--- src/services/bookstore-service.js
+++ src/services/bookstore-service.js
@@ -1,7 +1,10 @@
 export default class BookstoreService {
 
     getBooks() {
-        return [];
+        return [
+            {id: 1, title: 'Dracula', author: 'Bram Stoker'},
+            {id: 2, title: 'React Redux TypeScript', author: 'o Relly'}
+        ];
     }
 
 }
