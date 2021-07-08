23bb0e3893bcc241f685af032576533648445b84
kretgit <kret86@yandex.ru>
2021-07-08 22:57:15 +0300

    style files ready...

--- src/services/bookstore-service.js
+++ src/services/bookstore-service.js
@@ -2,8 +2,20 @@ export default class BookstoreService {
 
     getBooks() {
         return [
-            {id: 1, title: 'Dracula', author: 'Bram Stoker'},
-            {id: 2, title: 'React Redux TypeScript', author: 'o Relly'}
+            {
+                id: 1,
+                title: 'Dracula',
+                author: 'Bram Stoker',
+                price: 32,
+                coverImage: 'https://images-na.ssl-images-amazon.com/images/I/41yJ75gpV-L._SX381_BO1,204,203,200_.jpg'
+            },
+            {
+                id: 2,
+                title: 'React Redux TypeScript',
+                author: 'o Relly',
+                price: 45,
+                coverImage: 'https://images-na.ssl-images-amazon.com/images/I/414CRjLjwgL._SX403_BO1,204,203,200_.jpg'
+            }
         ];
     }
 
